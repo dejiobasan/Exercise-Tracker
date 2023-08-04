513162eaa0d3b681902d0a53b109f1092c8a56f2
Deji Obasan <dejiobasan@yahoo.com>
2023-08-04 22:29:02 +0100

    .

--- my-exercise-tracker/src/components/create-exercise.jsx
+++ my-exercise-tracker/src/components/create-exercise.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function CreateExercise() {
     const [user, setUser] = useState({
@@ -20,20 +22,31 @@ function CreateExercise() {
     }
 
     return (
-        <form className="row g-3" onSubmit={handleSubmit}>
-            <div className="col">
-                <input name="username" value={user.username} onChange={handleChange} type="text" className="form-control" placeholder="Username" />
-            </div>
-            <div className="col">
-                <input name="description" value={user.description} onChange={handleChange} type="text" className="form-control" placeholder="Exercise description" />
-            </div>
-            <div className="col">
-                <input name="duration" value={user.duration} onChange={handleChange} type="number" className="form-control" placeholder="duration" />
-            </div>
-            <div className="col-12">
-            <button type="submit" className="btn btn-dark">Submit</button>
-            </div>
-        </form>
+        <div>
+            <h3>Create a new Exercise Log</h3>
+            <form className="row g-3" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="form-label">Username: </label>
+                    <input name="username" value={user.username} onChange={handleChange} type="text" className="form-control" placeholder="Username" />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Exercise Description: </label>
+                    <input name="description" value={user.description} onChange={handleChange} type="text" className="form-control" placeholder="Exercise description" />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Duration: </label>
+                    <input name="duration" value={user.duration} onChange={handleChange} type="number" className="form-control" placeholder="duration" />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Date: </label>
+                    <DatePicker selected={user.date} onchange={handleChange} />
+                </div>
+                <div className="form-group">
+                <button type="submit" className="btn btn-dark">Create Exercise Log</button>
+                </div>
+            </form>
+        </div>
+        
     );
 }
 
--- my-exercise-tracker/src/components/create-user.jsx
+++ my-exercise-tracker/src/components/create-user.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateUser() {
+
+    const [newUser, setNewUser] = useState({
+        username: ""
+    });
+
+    function handleChange(event) {
+        const { value, name } = event.target;
+        setNewUser((prevState)=> ({...prevState,[name]:value})); 
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(newUser);
+        setNewUser({
+            username: ""
+        });
+    }
+
     return (
         <div>
-            <p>Your are on the Create Users Page!</p>
+            <h4>Create a Username </h4>
+            <form className="row g-3" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="form-label">Username: </label>
+                    <input name="username" value={newUser.username} onChange={handleChange} type="text" className="form-control" placeholder="Username" />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-dark">Create Username</button>
+                </div>
+            </form>
         </div>
     );
 }
